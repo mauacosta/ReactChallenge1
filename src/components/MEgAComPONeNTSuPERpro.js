44c9f3f06a2cacee5af0b681d7490eb7d4a6cb28
Mau Acosta <chafito@live.com>
2022-04-29 20:41:56 -0500

    Hola

---
+++ src/components/MEgAComPONeNTSuPERpro.js
@@ -0,0 +1,16 @@
+const MEgAComPONeNTSuPERpro = function () {
+  let spidermanGIF =
+    "https://c.tenor.com/Yi550gKa-lkAAAAS/spider-man-dance.gif";
+  return (
+    <div>
+      <h4>Mauricio Acosta Hernández</h4>
+      <h6>A01339392</h6>
+      <img
+        src="https://c.tenor.com/Yi550gKa-lkAAAAS/spider-man-dance.gif"
+        alt="Gif de spiderman"
+      />
+    </div>
+  );
+};
+
+export default MEgAComPONeNTSuPERpro;
